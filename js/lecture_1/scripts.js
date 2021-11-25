6071ce9a035e4438bb9276a9a1a1db45a18ad9ea
addams <addamsanna87@gmai.com>
2021-11-25 14:39:36 +0200

    lecture_1

---
+++ js/lecture_1/scripts.js
@@ -0,0 +1,10 @@
+// function delete (button1){
+//     const button1 =document.getElementById ("button1");
+
+// }
+document.getElementById("button1").onclick = function () {
+    document.getElementById("image").hidden = true;
+};
+document.getElementById("button2").onclick = function () {
+    document.getElementById("image").hidden = false;
+};
